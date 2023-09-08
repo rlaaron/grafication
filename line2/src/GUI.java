c1e31b7b6b791ba84a2502c9d94add0055627186
Aaron Rincon <rlaaron0305@gmail.com>
2023-09-07 21:03:50 -0600

    clean the function paint component

--- line2/src/GUI.java
+++ line2/src/GUI.java
@@ -36,50 +36,11 @@ class GUI extends JPanel {
         int x2 = (int) lineaRecta.endPoint.x;
         int y2 = (int) (lineaRecta.m * lineaRecta.endPoint.x + lineaRecta.b);
 
-        int dy = y2 - y1;
-        int dx = x2 - x1;
-        int DY =  dy/dy;
-        double DX =  dx/dy;
-
-        double decimal = 0;
-
-
-        // g2d.setColor(Color.BLACK);
-        // g2d.drawLine(x1, 600 - y1, x2 , 600 - y2 );
-
         g2d.setColor(Color.RED);
-        // for (Point point : selectedPoints) {
-        //     // subtract  500 to x and 600 to y to center the line
-        //     // g2d.fillRect((int) point.x - 600, (int) point.y - 600, 3, 3);
-        //     g2d.fillRect((int) point.x, (int) point.y, 3, 3);
-        // }
-
-       
-        //check if DX is >= 1 then keep the decimal part and add it to decimal, and then if the decimal is >= 1 add 1 to x1, all inside to a for loop, and then substract
-        // for(int i = 0; i < dy; i++){
-        //     decimal += DX;
-        //     if(decimal >= 1){
-        //         x1 += 1;
-        //         decimal -= 1;
-        //     }
-        //     y1 += DY;
-        //     // g2d.fillRect(x1 - 600, y1 - 600 , 3, 3);
-        //     // g2d.fillRect((int) point.x - 600, (int) point.y - 600, 3, 3);
-        //     g2d.fillRect(x1, 300 - y1, 3, 3);
-        // }
-
-        for(Point point : selectedPoints){
-            if(decimal >= 1){
-                x1 += 1;
-                decimal -= 1;
-            }else{
-                // int newY = 600 - (int) point.y;
-                g2d.fillRect((int) point.x, 600 - (int) point.y  , 3, 3);
-            }
+        for (Point point : selectedPoints) {
+            // subtract  600 to point.y because the origin of the screen is at the top left corner
+            g2d.fillRect((int) point.x, 600 - (int) point.y, 3, 3);
         }
-
-
-
         
     }
 }
